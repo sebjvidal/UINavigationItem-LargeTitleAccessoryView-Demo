953090c74d0a435151818b4f984869c5d26c5a10
sebjvidal <seb.vidal@btinternet.com>
2023-11-18 14:04:51 +0000

    Initial Commit

---
+++ UINavigationItem-LargeTitleAccessoryView-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UINavigationItem-LargeTitleAccessoryView-Demo
+//
+//  Created by Seb Vidal on 18/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
